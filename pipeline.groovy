6e6246c08f846f63a7ee2da5e5ca7d442350657f
sivaganesan23 <45412181+sivaganesan23@users.noreply.github.com>
2019-03-10 13:35:42 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,6 +6,6 @@ node('SLAVE') {
         sh 'mvn compile'
     }
     stage('Quality Check') {
-        sh 'mvn sonar:sonar -Dsonar.projectKey=sivaganesan23_studentproj-code -Dsonar.organization=sivaganesan23-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=21e8fdbec9796ec78e5ddcfeb2c1cd0659289e5b')
+        sh 'mvn sonar:sonar -Dsonar.projectKey=sivaganesan23_studentproj-code -Dsonar.organization=sivaganesan23-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=21e8fdbec9796ec78e5ddcfeb2c1cd0659289e5b'
     }    
 }
