693ea1f838687f2d037ad87021117eefdab68d55
sivaganesan23 <45412181+sivaganesan23@users.noreply.github.com>
2019-03-11 22:02:53 +0530

    Create prod-seed.groovy

---
+++ prod-seed.groovy
@@ -0,0 +1,27 @@
+pipelineJob('seed-prod-pipe_converted') {
+  description('')
+  displayName('seed-pipe_converted')
+  configure { flowdefinition ->
+
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'configVersion'(2)
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/sivaganesan23/pipeline.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/master')
+          }
+        }
+        'doGenerateSubmoduleConfigurations'(false)
+        'submoduleCfg'(class:'list')
+        'extensions'()
+      }
+      'scriptPath'('prod-pipeline.groovy')
+      'lightweight'(true)
+    }
+  }
+}
