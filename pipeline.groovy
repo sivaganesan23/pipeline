704a57a3bc405127b1d0f3ac801d92b4d7e722a5
sivaganesan23 <45412181+sivaganesan23@users.noreply.github.com>
2019-03-11 19:33:43 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,16 +3,16 @@ node('SLAVE') {
         git 'https://github.com/sivaganesan23/studentproj-code.git'
     }
     stage('Compile') {
-        sh 'mvn compile'
+        sh 'mvn compile -D VERSION=$VERSION -D TYPE=$VERSIONTYPE '
     }
     stage('Quality Check') {
-        sh 'mvn sonar:sonar -Dsonar.projectKey=sivaganesan23_studentproj-code -Dsonar.organization=sivaganesan23-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=21e8fdbec9796ec78e5ddcfeb2c1cd0659289e5b'
+        sh 'mvn sonar:sonar -Dsonar.projectKey=sivaganesan23_studentproj-code -Dsonar.organization=sivaganesan23-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=21e8fdbec9796ec78e5ddcfeb2c1cd0659289e5b -D VERSION=$VERSION -D TYPE=$VERSIONTYPE'
     }    
     stage('Package') {
-        sh 'mvn package'
+        sh 'mvn package -D VERSION=$VERSION -D TYPE=$VERSIONTYPE'
     }
     stage('Upload Artifacts') {
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'NEXUS-USER-PASS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-        sh 'mvn deploy -D NEXUS_USERNAME=$USERNAME -D NEXUS_PASSWORD=$PASSWORD -D VERSION=$VERSIONNO -D TYPE=$VERSIONTYPE'
+        sh 'mvn deploy -D NEXUS_USERNAME=$USERNAME -D NEXUS_PASSWORD=$PASSWORD -D VERSION=$VERSION -D TYPE=$VERSIONTYPE'
     }        
 }
